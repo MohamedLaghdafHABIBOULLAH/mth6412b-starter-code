b03f9718c7d6c279678fd5d734e52c82cc89be21
MohamedLaghdafHABIBOULLAH <laghdaf.hacen@gmail.com>
2023-10-23 11:09:00 -0400

    Create connexe.jl

---
+++ projet/phase1/connexe.jl
@@ -0,0 +1,21 @@
+include("node.jl")
+
+
+
+
+
+
+
+mutable struct BinaryTree{T} <: AbstractNode{T} name::String
+     data::T
+     left::Union{BinaryTree{T}, Nothing}
+     right::Union{BinaryTree{T}, Nothing}
+     parent::Union{BinaryTree{T}, Nothing}
+end
+function BinaryTree(data::T; name::String="",
+left::Union{BinaryTree{T}, Nothing}=nothing, right::Union{BinaryTree{T}, Nothing}=nothing, parent::Union{BinaryTree{T}, Nothing}=nothing) where T
+     BinaryTree(name, data, left, right, parent)
+end
+ left(bt::BinaryTree) = bt.left
+ right(bt::BinaryTree) = bt.right
+ parent(bt::BinaryTree) = bt.parent
